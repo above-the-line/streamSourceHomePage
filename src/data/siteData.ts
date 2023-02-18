f29bb1e393d700dd9a52505050fcc5e8f267d26c
Avi Michael <play.right.productions@gmail.com>
2023-02-19 10:17:38 +1100

    added tailwindcss and images

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -3,7 +3,7 @@ import { FrontPageSliderCard } from "../types/types.d";
 export { };
 
 export var cards: FrontPageSliderCard[] = [
-    { order: 1, title: 'Best thing 1', image: '34.jpg' },
+    { order: 1, title: 'Best thing 1', image: './public/images/cards/rigFront.jpg' },
     { order: 2, title: 'Best thing 2', image: '34.jpg' },
     { order: 3, title: 'Best thing 3', image: '34.jpg' },
     { order: 4, title: 'Best thing 4', image: '34.jpg' }
